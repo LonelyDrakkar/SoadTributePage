2b5978b658342dd7225d352b29f677ec6586dba4
Mario Jaque <mjaqueg1@gmail.com>
2022-04-18 17:42:44 -0400

    Navigation buttom structure updated.

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,16 @@
-// Pending
+window.onload = () => {
+    const checker = document.querySelector('#label');
+    const checkbox = document.querySelector('#check');
+
+    checker.addEventListener('click', (event) => {
+        const newNav = document.querySelector('#links-container');
+
+        if(checkbox.checked === false){
+            newNav.style.cssText += 'right: 0;';
+            checker.style.cssText += 'opacity: 0;';
+        }else{
+            newNav.style.cssText += 'right: -100vw';
+            checker.style.cssText += 'opacity: 1;';
+        }
+    });
+}
