2402240802a0d560658d3d03bb65f6f7956b377b
Mario Jaque <mjaqueg1@gmail.com>
2022-04-18 18:03:25 -0400

    Navbar functionality added.

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,24 @@
 window.onload = () => {
     const checker = document.querySelector('#label');
     const checkbox = document.querySelector('#check');
+    const newNav = document.querySelector('#links-container');
 
-    checker.addEventListener('click', (event) => {
-        const newNav = document.querySelector('#links-container');
+    checker.addEventListener('click', () => {
 
         if(checkbox.checked === false){
             newNav.style.cssText += 'right: 0;';
-            checker.style.cssText += 'opacity: 0;';
+            checker.classList.toggle('toggle');
         }else{
             newNav.style.cssText += 'right: -100vw';
-            checker.style.cssText += 'opacity: 1;';
+            checker.classList.toggle('toggle');
+        }
+    });
+
+    newNav.addEventListener('click', () => {
+        if(checkbox.checked === true){
+            newNav.style.cssText += 'right: -100vw';
+            checker.classList.toggle('toggle');
+            checkbox.checked = false;
         }
     });
 }
